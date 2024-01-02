bf2e3353e25b2a560833077ebfe9130100b114c4
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-01-02 18:28:56 -0500

    planet

--- lib/planets.py
+++ lib/planets.py
@@ -1,4 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from . import Base
 from big_round_thing import big_round_thing
+
+class Planet(Base):
+    __tablename__ = 'planets'
+
+    id = Column(Integer, primary_key=True, index=True)
+    star_id = Column(Integer)
+    name = Column(String, index=True)
+    terrain = Column(String)
+    atmosphere = Column(String)
+    has_colony = Column(Boolean)
+    population = Column(Integer)
+    big_round_thing_id = Column(Integer)
+
+    big_round_thing = relationship('BigRoundThing', back_populates='planet')
 
 class planet(big_round_thing):
     def __init__(self,name,terrain,atmosphere,has_colony):
