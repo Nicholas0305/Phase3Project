9ecf0c9f90e2b63f091004459d6caa7c95fba26c
Evan Losh <evand.losh@gmail.com>
2024-01-02 07:52:55 -0500

    filling out classes

--- lib/big_round_thing.py
+++ lib/big_round_thing.py
@@ -1,2 +1,22 @@
 class big_round_thing:
-    pass
+
+    all = []
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if not isinstance(name, str):
+            raise ValueError("names must be strings.")
+        elif (name in [thing.name for thing in big_round_thing.all]):
+            raise ValueError("that name is already taken.")
+        else:
+            self._name = name
+            big_round_thing.all.append(self)
+
+    name = property(get_name, set_name)
+
+    def delete(self):
+        big_round_thing.all.remove(self)
---
+++ lib/test_classes.py
@@ -0,0 +1,10 @@
+from planets import planet
+from stars import Star
+from big_round_thing import big_round_thing
+import ipdb
+
+star1 = Star("The Sun")
+planet1 = planet("Mars", "Rocky", "thin", False) 
+planet2 = planet("Earth", "Rocky", "nice", True) 
+
+ipdb.set_trace()
