41e9630e6cde8f85e34861515bdf97aee2a729e2
Evan Losh <evand.losh@gmail.com>
2024-01-02 06:53:09 -0500

    pulled

---
+++ lib/planets.py
@@ -0,0 +1,8 @@
+
+
+class planet:
+    def __init__(self,name,terrain,atmosphere,has_colony):
+        self.name = name
+        self.terrain = terrain
+        self.atmosphere = atmosphere
+        self.has_colony = has_colony
