a106dffe6c49c7c00309df3d8693821a11ce087d
Nicholas <najernigin@gmail.com>
2024-01-03 14:59:58 -0600

    Boxes

--- lib/main.py
+++ lib/main.py
@@ -9,25 +9,33 @@ def main(stdscr):
     stdscr.refresh()
 
     # Create a window for the Textbox
-    win = curses.newwin(13, 38, 2, 2)
-    box = Textbox(win)
+    
+    planet_win = curses.newwin(13, 38, 2, 2)
 
     # Draw a box around the Textbox
     rectangle(stdscr, 1, 1, 15, 40)
     stdscr.refresh()
 
-    # Let the user edit the Textbox
-    box.edit()
-
-    # Get the contents of the Textbox
-    contents = box.gather()
-
     # Print the contents
-    stdscr.addstr(10, 2, "You entered: " + contents)
+    stdscr.addstr(5,5,"Planet PlaceHolder")
     stdscr.refresh()
+
+    # Create a second window below planet_win
+    second_win = curses.newwin(10, 30, 20, 2)
+    second_box = Textbox(second_win)
+
+    # Draw a box around the second_win
+    rectangle(stdscr, 18, 1, 12, 32)
+    stdscr.refresh()
+
+    # Let the user edit the second_box
+    second_box.edit()
+
+    # Get the contents of the second_box
+    second_contents = second_box.gather()
 
     # Wait for a key press before exiting
     stdscr.getch()
 
 # Run the application using curses.wrapper
-curses.wrapper(main)
+wrapper(main)
