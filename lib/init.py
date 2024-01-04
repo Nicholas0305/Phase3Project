6029eda6946ccf9032057213c9f544d911cf0d0f
sean innella <sean.innella@gmail.com>
2024-01-04 11:40:10 -0500

    cleaned init.py

--- lib/init.py
+++ lib/init.py
@@ -1,9 +1,4 @@
-#sean's code
-#from .base import Base
-#from .big_round_thing import BigRoundThing
-#from .star import Star
-#from .planet import Planet
-#from .solar_system import SolarSystem
+
 import sqlite3
 
 CONN = sqlite3.connect('database.db')
