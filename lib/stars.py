56d5ad3901e03ea12e50d291442b90dd08f46d28
sean innella <sean.innella@gmail.com>
2024-01-04 11:41:02 -0500

    cleaned stars.py

--- lib/stars.py
+++ lib/stars.py
@@ -69,24 +69,3 @@ class Star(big_round_thing):
         """
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
-
-
-    
-
-
-
-#sean's code
-#from sqlalchemy import Column, Integer, ForeignKey
-#from sqlalchemy.orm import relationship
-#from base import Base
-#from big_round_thing import BigRoundThing
-
-#class Star(BigRoundThing):
-#    __tablename__ = 'stars'
-
-#    id = Column(Integer, ForeignKey('big_round_things.id'), primary_key=True)
-#    solar_system_id = Column(Integer, ForeignKey('solar_systems.id'))
-
-#    __mapper_args__ = {
-#        'polymorphic_identity':'stars',
-#    }
