cf3f74ec7bb149c4f327dbb6787988c6db3c5a98
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-01-04 16:13:49 -0500

    planets

--- lib/planets.py
+++ lib/planets.py
@@ -1,48 +1,10 @@
-from sqlalchemy import Column, Integer, String, Boolean, create_engine
-from sqlalchemy.orm import relationship, sessionmaker
-from . import Base
 from big_round_thing import big_round_thing
+from init import CURSOR, CONN
 from stars import Star
 
-class Planet(Base):
-    __tablename__ = 'planets'
+class Planet(big_round_thing):
+    all = []
 
-    id = Column(Integer, primary_key=True, index=True)
-    star_id = Column(Integer)
-    name = Column(String, index=True)
-    terrain = Column(String)
-    atmosphere = Column(String)
-    has_colony = Column(Boolean)
-    population = Column(Integer)
-
-    @classmethod
-    def create_table(cls):
-        Base.metadata.create_all(bind=create_engine)
-
-    @classmethod
-    def drop_table(cls):
-        Base.metadata.tables['planets'].drop(bind=create_engine)
-    
-    def save(self, session):
-        session.add(self)
-        session.commit()
-    
-    def update(self, session):
-        session.commit()
-    
-    def delete(self, session):
-        session.delete(self)
-        session.commit()
-    
-    @classmethod
-    def get_planets(cls, session):
-        return session.query(cls).all()
-    
-    @classmethod
-    def get_planet_by_name(cls, session, name):
-        return session.query(cls).filter_by(name=name).first()
-
-class planet(big_round_thing):
     def __init__(self, name, terrain, atmosphere, has_colony, star, id = None):
         super().__init__(name)
         self.terrain = terrain
@@ -50,6 +12,94 @@ class planet(big_round_thing):
         self.has_colony = has_colony
         self.star = star
         self.id = id
+    
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS planets (
+                id INTEGER PRIMARY KEY,
+                star_id INTEGER,
+                name TEXT,
+                terrain TEXT,
+                atmosphere TEXT,
+                has_colony BOOLEAN,
+                population INTEGER,
+                big_round_thing_id INTEGER
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit
+    
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS planets;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    def save(self):
+        sql = """
+            INSERT INTO planets (star_id, name, terrain, atmosphere, has_colony, population, big_round_thing_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """
+        values = (self.star.id, self.name, self.terrain, self.atmosphere, self.has_colony, self.population, self.big_round_thing_id)
+        CURSOR.execute(sql, values)
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+    
+    @classmethod
+    def create_planet(cls, name, terrain, atmosphere, has_colony, star):
+        planet = cls(name, terrain, atmosphere, has_colony, star)
+        planet.save()
+        return planet
+    
+    def update(self):
+        sql = """
+            UPDATE planets
+            SET star_id = ?, name = ?, terrain = ?, atmosphere = ?,
+                has_colony = ?, population = ?, big_round_thing_id = ?
+            WHERE id = ?
+        """
+        values = (self.star.id, self.name, self.terrain, self.atmosphere, self.has_colony, self.population, self.big_round_thing_id, self.id)
+        CURSOR.execute(sql, values)
+        CONN.commit()
+    
+    def delete(self):
+        sql = """
+            DELETE FROM planets
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit
+    
+    @classmethod
+    def get_planets(cls):
+        sql = """
+            SELECT * FROM planets;
+        """
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        planets = []
+
+        for row in rows:
+            planet = cls(*row)
+            planets.append(planet)
+        return planets
+    
+    @classmethod
+    def get_planets_by_name(cls, name):
+        sql = """
+            SELECT * FROM planets WHERE name = ?;
+        """
+        CURSOR.execute(sql, (name,))
+        row = CURSOR.fetchone()
+
+        if row:
+            planet = cls(*row)
+            return planet
+        else:
+            return None
 
     # Terrain property
     def get_terrain(self):
